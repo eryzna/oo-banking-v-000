03f3402861291e0aa1795e98d7b0a187617c6493
Elana Ryznar <eryzna@gmail.com>
2018-06-15 03:46:58 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
+  attr_accessor :balance, :status, :amount
+  attr_reader :name
+
+  def initialize(name, balance=1000, status="open")
+    @name=name
+    @balance=balance
+    @status=status
+  end
+
+  def deposit(amount)
+    self.balance+=amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if status=="open" && balance >0
+      self.valid?=true
+    else
+      !self.valid?
+    end
+  end
 
 end
