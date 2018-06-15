b407d2b398fff071f20dda209a196c412cf911d9
Elana Ryznar <eryzna@gmail.com>
2018-06-15 04:28:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,9 +14,11 @@ class Transfer
   end
 
   def execute_transaction
-    sender.balance -= amount
-    receiver.balance += amount
-    self.status="complete"
+    if sender.valid?
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status="complete"
+    end
   end
 
 end
